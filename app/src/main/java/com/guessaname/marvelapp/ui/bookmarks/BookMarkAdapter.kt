54aff485c3c1f07f5389fc37eff230e712119fa6
lucadilorenzo <luca.dilorenzo98@gmail.com>
2021-07-01 18:42:14 +0200

    rimosse cose inutili

--- app/src/main/java/com/guessaname/marvelapp/ui/bookmarks/BookMarkAdapter.kt
+++ app/src/main/java/com/guessaname/marvelapp/ui/bookmarks/BookMarkAdapter.kt
@@ -1,18 +1,11 @@
 package com.guessaname.marvelapp.ui.bookmarks
 
-import android.app.Activity
-import android.app.PendingIntent.getActivity
-import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import android.os.Bundle
-import android.widget.Toast
-import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
-import com.guessaname.marvelapp.MainActivity
 import com.guessaname.marvelapp.R
 import com.guessaname.marvelapp.ScrollingActivity
 
@@ -37,25 +30,16 @@ class BookMarkAdapter(private val listener: RecyclerView): RecyclerView.Adapter<
         return names.size
     }
 
-    inner class BookMarkViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView),
-        View.OnClickListener {
+    inner class BookMarkViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView){
 
         var itemNames: TextView = itemView.findViewById(R.id.tv_character_name)
         var itemDescription: TextView = itemView.findViewById(R.id.tv_character_short_bio)
 
-
-
         init { itemView.setOnClickListener {
-            //val position: Int = adapterPosition
-            //Toast.makeText(itemView.context, "you clicked on item $position", Toast.LENGTH_SHORT).show()
             val context = itemView.context
             val intent = Intent(context, ScrollingActivity::class.java).apply{}
             context.startActivity(intent)
             }
-        }
-
-        override fun onClick(v: View?) {
-            TODO("Not yet implemented")
         }
     }
 }
--- app/src/main/java/com/guessaname/marvelapp/ui/bookmarks/BookMarkFragment.kt
+++ app/src/main/java/com/guessaname/marvelapp/ui/bookmarks/BookMarkFragment.kt
@@ -4,17 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.guessaname.marvelapp.R
 import kotlinx.android.synthetic.main.fragment_bookmark.*
-import okhttp3.internal.notify
 
-
-class BookMarkFragment : Fragment(), AdapterView.OnItemClickListener {
+class BookMarkFragment : Fragment(){
 
     private var layoutManager: RecyclerView.LayoutManager? = null
     private var adapter: RecyclerView.Adapter<BookMarkAdapter.BookMarkViewHolder>? = null
@@ -33,9 +29,5 @@ class BookMarkFragment : Fragment(), AdapterView.OnItemClickListener {
             layoutManager = LinearLayoutManager(activity)
             adapter = BookMarkAdapter(this)
         }
-    }
-
-    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        //Toast.makeText(this, "Item $position clicked",Toast.LENGTH_SHORT).show()
     }
 }
