c25d5911322bfb754402cd2b5e425a83f345ce37
luke9598 <moretti.luca1998@gmail.com>
2021-07-01 18:49:58 +0200

    non entra la repo

--- app/src/main/java/com/guessaname/marvelapp/ui/bookmarks/BookMarkAdapter.kt
+++ app/src/main/java/com/guessaname/marvelapp/ui/bookmarks/BookMarkAdapter.kt
@@ -1,23 +1,16 @@
 package com.guessaname.marvelapp.ui.bookmarks
 
-import android.app.Activity
-import android.app.PendingIntent.getActivity
-import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import android.os.Bundle
-import android.widget.Toast
-import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
-import com.guessaname.marvelapp.MainActivity
 import com.guessaname.marvelapp.R
 import com.guessaname.marvelapp.ScrollingActivity
 
 
-class BookMarkAdapter(private val listener: RecyclerView): RecyclerView.Adapter<BookMarkAdapter.BookMarkViewHolder> () {
+class BookMarkAdapter : RecyclerView.Adapter<BookMarkAdapter.BookMarkViewHolder> () {
 
     // examples, we need to attach DB
     private val names = arrayOf("Thor", "SpiderMan", "BatMan", "IronMan")
@@ -37,25 +30,19 @@ class BookMarkAdapter(private val listener: RecyclerView): RecyclerView.Adapter<
         return names.size
     }
 
-    inner class BookMarkViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView),
-        View.OnClickListener {
+    inner class BookMarkViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView){
 
         var itemNames: TextView = itemView.findViewById(R.id.tv_character_name)
         var itemDescription: TextView = itemView.findViewById(R.id.tv_character_short_bio)
 
-
-
-        init { itemView.setOnClickListener {
+        init {
+            itemView.setOnClickListener {
             //val position: Int = adapterPosition
-            //Toast.makeText(itemView.context, "you clicked on item $position", Toast.LENGTH_SHORT).show()
             val context = itemView.context
             val intent = Intent(context, ScrollingActivity::class.java).apply{}
             context.startActivity(intent)
             }
         }
 
-        override fun onClick(v: View?) {
-            TODO("Not yet implemented")
-        }
     }
 }
--- app/src/main/java/com/guessaname/marvelapp/ui/bookmarks/BookMarkFragment.kt
+++ app/src/main/java/com/guessaname/marvelapp/ui/bookmarks/BookMarkFragment.kt
@@ -4,20 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.guessaname.marvelapp.R
 import kotlinx.android.synthetic.main.fragment_bookmark.*
-import okhttp3.internal.notify
 
 
-class BookMarkFragment : Fragment(), AdapterView.OnItemClickListener {
 
-    private var layoutManager: RecyclerView.LayoutManager? = null
-    private var adapter: RecyclerView.Adapter<BookMarkAdapter.BookMarkViewHolder>? = null
+class BookMarkFragment : Fragment(){
+
+    //private var layoutManager: RecyclerView.LayoutManager? = null
+    //private var adapter: RecyclerView.Adapter<BookMarkAdapter.BookMarkViewHolder>? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,11 +28,9 @@ class BookMarkFragment : Fragment(), AdapterView.OnItemClickListener {
         super.onViewCreated(itemView, savedInstanceState)
         bookmarks_recyclerview.apply{
             layoutManager = LinearLayoutManager(activity)
-            adapter = BookMarkAdapter(this)
+            adapter = BookMarkAdapter()
         }
     }
 
-    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        //Toast.makeText(this, "Item $position clicked",Toast.LENGTH_SHORT).show()
-    }
+
 }
