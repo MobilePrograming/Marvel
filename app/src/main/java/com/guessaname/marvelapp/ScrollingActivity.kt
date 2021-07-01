d9390c44add8ca35c23e93c8a425ff54b0dc941b
lucadilorenzo <luca.dilorenzo98@gmail.com>
2021-07-01 19:07:09 +0200

    remove test

--- app/src/main/java/com/guessaname/marvelapp/ScrollingActivity.kt
+++ app/src/main/java/com/guessaname/marvelapp/ScrollingActivity.kt
@@ -19,8 +19,6 @@ class ScrollingActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.toolbar))
         binding.toolbarLayout.title = title
 
-        // TEST
-
         //Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(app_bar.background);
     }
 }
